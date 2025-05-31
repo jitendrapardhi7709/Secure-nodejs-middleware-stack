36c714d589ee4d430b07482aeed1432112216edd
jitendrapardhi7694 <pardhijitendra7694@gmail.com>
2025-05-31 15:38:41 +0530

    updated the code of the Secure Node.js Middleware Stack

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -21,6 +21,11 @@ exports.addToBlacklist = [
   (req, res) => {
     const { ip } = req.body;
     if (!ip) return res.status(400).json({ error: 'IP address required' });
+
+    if (blacklistedIPs.has(ip)) {
+      return res.status(409).json({ message: `IP ${ip} is already in the blacklist` });
+    }
+
     blacklistedIPs.add(ip);
     res.json({ message: `IP ${ip} added to blacklist` });
   },
@@ -31,6 +36,11 @@ exports.removeFromBlacklist = [
   (req, res) => {
     const { ip } = req.body;
     if (!ip) return res.status(400).json({ error: 'IP address required' });
+
+    if (!blacklistedIPs.has(ip)) {
+      return res.status(404).json({ error: `IP ${ip} is not in the blacklist` });
+    }
+
     blacklistedIPs.delete(ip);
     res.json({ message: `IP ${ip} removed from blacklist` });
   },
